f3f84aa73998a47f222f2a4c32010316710c5f34
Maria Tentea <mariatentea3@gmail.com>
2023-10-10 14:05:00 +0300

    Problem2 test verified

--- Lab1/src/Problem2/NumbersArrayTest.java
+++ Lab1/src/Problem2/NumbersArrayTest.java
@@ -16,10 +16,27 @@ public class NumbersArrayTest {
         NumbersArray n1 = new NumbersArray();
         int[] array = new int[]{1, 2, 3, 4};
         assert n1.minSum(array) == 6;
-        assert n1.maxSum(array) == 10;
+        assert n1.maxSum(array) == 9;
         //wrong test:
         //assert n1.minSum(array) == 16;
         //assert n1.maxSum(array) == 1;
+
+    }
+
+    public void minAndMaxTestFalse(){
+        NumbersArray n1 = new NumbersArray();
+        int[] array = new int[]{10, 5, 132, 45, 8};
+        //wrong test:
+         assert n1.maxNr(array) == 10;
+        assert n1.minNr(array) == 8;
+    }
+
+    public void minAndMaxSumFalse(){
+        NumbersArray n1 = new NumbersArray();
+        int[] array = new int[]{1, 2, 3, 4};
+        //wrong test:
+        assert n1.minSum(array) == 16;
+        assert n1.maxSum(array) == 1;
 
     }
 
